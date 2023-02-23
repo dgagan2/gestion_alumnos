b5690b135df9d5b711ed62284681744aeda43698
dgagan2 <andgag@duck.com>
2023-02-22 22:54:56 -0500

    up

--- js/gAcademica.js
+++ js/gAcademica.js
@@ -1,6 +1,6 @@
 $(function(){
   $('.materias').hide();
-  $('#tblalumnos').hide()
+  //$('#tblalumnos').hide()
     $('#btnPAlumno').click(function(){
       $('.materias').hide();
       $('.promedios').show();
@@ -10,9 +10,19 @@ $(function(){
       $('.promedios').hide();
     })
 })
+const form=document.querySelector('#formAlu').addEventListener("submit", function(event){
+  event.preventDefault();
+  let transform=new FormData(form)
+  
+})
 const notas=[]//arreglo notas
 let Ntemp=JSON.parse(localStorage.getItem("arrayNotas"))
 Array.prototype.push.apply(notas,Ntemp)//arreglo notas
+
+const assclasstogroup=[]//arreglo de asignacion de materias a grupos
+var b=JSON.parse(localStorage.getItem("arrayclasstogroup"))
+Array.prototype.push.apply(assclasstogroup,b)//arreglo agregar materias a grupo
+
 function borartb(){
   d=document.getElementById("tbodyAlumnos")
   e=document.getElementById("pPromedio")
@@ -32,18 +42,20 @@ opcion.addEventListener('change', ()=>{
     $('.container-prom-alumno').show();
     $('.container-prom-grupo').hide();
   }else{
-    $('.container-prom-grupo').show();
+    $('.container-prom-grupo').show(tablaProGrupo());
     $('.container-prom-alumno').hide();
     $('#tblalumnos').hide();    
+    document.getElementById("tbodyAlumnos").innerHTML = ''
+    document.getElementById("promedio").innerHTML = ''
+
   }
 })
 let arrtemp=[]
 document.querySelector("#btnfindID").addEventListener("click", function(){
-  
   let IdFind=document.querySelector("#txtId").value;
   if(IdFind!==""){
-    let sum = 0;
     for(var j=0;j<notas.length;j++){
+
         if(IdFind==notas[j].Id){
           total=notas[j].Calificacion;
           arrtemp.push(total)
@@ -62,15 +74,63 @@ document.querySelector("#btnfindID").addEventListener("click", function(){
         }else{
           alert("No tiene cursos matriculados")
         }
-        $('#tblalumnos').show(); 
+        // arrtemp.forEach(myFunction);
+        // function myFunction(item) {sum += parseFloat(item);}
+        $('#tblalumnos').show(c()); 
+        
     }
-    arrtemp.forEach(myFunction);
-    function myFunction(item) {sum += parseFloat(item);}
-    document.querySelector("#promedio").innerHTML+=
-        `
-        <p id="pPromedio">Promedio: ${sum}</p>
-        `   
   }else{
     alert("Imgrese el ID del estudiante")
   }
 })
+function c(){
+  let sum = 0;
+  arrtemp.forEach(myFunction);
+  function myFunction(item) {sum += parseFloat(item);}
+  document.querySelector("#promedio").innerHTML+=
+    `
+    <p id="pPromedio">Promedio: ${sum}</p>
+    `
+  arrtemp.pop()
+  document.getElementById("txtId").value=""
+}
+let totalProGrupo=[]
+function tablaProGrupo(){
+  for(var j=0;j<assclasstogroup.length;j++){
+    // document.querySelector("#tblgrupos").innerHTML+=
+    //     `
+    //     <tbody id="tbodyAlumnos">
+    //         <tr>
+    //           <td>${assclasstogroup[j].Grupo}</td>
+    //           <td>${assclasstogroup[j].Clase}</td>
+    //         </tr>
+    //     </tbody>
+    //     `
+    for(var i=0;i<notas.length;i++){
+      if(assclasstogroup[j].Grupo == notas[i].Grupo){
+        savegr=notas[i].Grupo
+        totalG=notas[i].Calificacion
+        totalProGrupo.push({Grupo:savegr,Calificacion:totalG})
+      }else{
+        alert("nada")
+      }
+    }
+    }obtenerPor()
+    
+}
+function obtenerPor(){
+  for(var j=0;j<totalProGrupo.length;j++){
+    let contador=0
+    if(totalProGrupo[j].Grupo == totalProGrupo[j].Grupo){
+      
+      alert("hola"+contador)
+      let sumaCali=0
+      let h=parseFloat(totalProGrupo[j].Calificacion)
+      let sumaCali2=h+sumaCali
+      console.log(contador,sumaCali2)
+    }else{
+
+    }
+
+  }
+}
